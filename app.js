4f5ac0592c104c2bf122a4badafd4b8b7128512f
cabrel jordan <ncjjjk1223@gmail.com>
2026-02-02 03:42:50 +0100

    version 1

--- app.js
+++ app.js
@@ -1,13 +1,47 @@
 const btn=document.querySelector('.pokemon_btn')
 const name_poke=document.querySelector('#pokemon')
-
+const info=document.querySelector('.info')
+const img=document.querySelector('#img_pokemon')
+const ch1=document.querySelector('.choix1')
+const ch2=document.querySelector('.choix2')
+ch1.addEventListener('click',async ()=>{
+    const nb_ale= Math.floor(Math.random() * (1349 - 2 + 1)) + 2; 
+    fetch(`https://pokeapi.co/api/v2/pokemon/${nb_ale}`)
+        .then(rps=>{
+            console.log(rps.ok);
+            
+            if (!rps.ok) {
+                throw new Error ('pokemon inexsitant')
+            }
+            return rps.json()
+        }
+        )
+        .then(data=>
+            {
+                img.src=data.sprites.back_default
+                img.classList.remove('hidden')
+                info.textContent=`name: ${data.name}`
+            })
+        .catch(error=>console.error(error))
+})
 
 btn.addEventListener('click',async ()=>{
-    // console.log(`pokemon/${name_poke.value}`);
-    
     fetch(`https://pokeapi.co/api/v2/pokemon/${name_poke.value}`)
-        .then(rps=>rps.json())
-        .then(data=>console.log(data.sprites.back_default))
+        .then(rps=>{
+            console.log(rps.ok);
+            
+            if (!rps.ok) {
+                throw new Error ('pokemon inexsitant')
+            }
+            return rps.json()
+        }
+        )
+        .then(data=>
+            {
+                img.src=data.sprites.back_default
+                img.classList.remove('hidden')
+                info.textContent=`name: ${data.name}`
+            })
         .catch(error=>console.error(error))
 }
 )
