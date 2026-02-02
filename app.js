86f0712917769fa02710caa7704e0f29ebae15e6
cabrel jordan <ncjjjk1223@gmail.com>
2026-02-02 02:39:23 +0100

    formatage des donne

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-console.log('ok');
-async function call_api(){
-    const rqt=await fetch("https://pokeapi.co/api/v2/pokemon/Dragonite")
-    const rsp= await rqt.json()
-    console.log(rsp);
-    console.log('dfg');
-}
-call_api()
+const btn=document.querySelector('.pokemon_btn')
+const name_poke=document.querySelector('#pokemon')
 
-console.log(6);
+
+btn.addEventListener('click',async ()=>{
+    // console.log(`pokemon/${name_poke.value}`);
+    
+    fetch(`https://pokeapi.co/api/v2/pokemon/${name_poke.value}`)
+        .then(rps=>rps.json())
+        .then(data=>console.log(data.sprites.back_default))
+        .catch(error=>console.error(error))
+}
+)
+
